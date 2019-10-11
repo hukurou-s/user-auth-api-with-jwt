249770842032b8bf0dc9c9ba9b7583ac3bbb0ce9
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2019-10-11 20:10:14 +0900

    implement access restriction using jwt

--- infrastructure/router.go
+++ infrastructure/router.go
@@ -1,6 +1,9 @@
 package infrastructure
 
 import (
+	"io/ioutil"
+
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 
@@ -18,8 +21,24 @@ func init() {
 
 	controller := controllers.NewUserController(NewSqlHandler())
 
-	e.POST("/user/login", func(c echo.Context) error { return controller.Login(c) })
-	e.POST("/user/create", func(c echo.Context) error { return controller.Create(c) })
+	e.POST("/login", func(c echo.Context) error { return controller.Login(c) })
+
+	pubKeyData, err := ioutil.ReadFile("./rsa/id_rsa.pub.pkcs8")
+	if err != nil {
+		panic(err)
+	}
+
+	pubKey, err := jwt.ParseRSAPublicKeyFromPEM(pubKeyData)
+	if err != nil {
+		panic(err)
+	}
+
+	u := e.Group("/user")
+	u.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+		SigningKey:    pubKey,
+		SigningMethod: "RS256",
+	}))
+	u.POST("/create", func(c echo.Context) error { return controller.Create(c) })
 
 	Echo = e
 }
