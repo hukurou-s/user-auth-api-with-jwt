9a70dcaca84f2905fdc2284f5627e80385620c58
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2019-10-11 17:55:14 +0900

    create user entity

---
+++ domain/user.go
@@ -0,0 +1,15 @@
+package domain
+
+import "github.com/jinzhu/gorm"
+
+type User struct {
+	gorm.Model
+	Snum     string `gorm:"size:32;unique;not null" json:"snum"`
+	Name     string `gorm:"size:255;unique;not null" json:"name"`
+	Password string `gorm:"size:255;not null" json:"password"`
+}
+
+type LoginParams struct {
+	Snum     string `json:"snum"`
+	Password string `json:"password"`
+}
